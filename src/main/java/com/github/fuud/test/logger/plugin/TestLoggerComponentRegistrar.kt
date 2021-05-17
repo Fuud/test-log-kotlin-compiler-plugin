25bce35b893e000eec72af04cd9518aafb16c49b
Fedor Bobin <fuudtorrentsru@gmail.com>
2021-05-17 21:50:57 +0300

    register as a service

--- src/main/java/com/github/fuud/test/logger/plugin/TestLoggerComponentRegistrar.kt
+++ src/main/java/com/github/fuud/test/logger/plugin/TestLoggerComponentRegistrar.kt
@@ -1,5 +1,6 @@
 package com.github.fuud.test.logger.plugin
 
+import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
@@ -7,6 +8,7 @@ import org.jetbrains.kotlin.com.intellij.mock.MockProject
 import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
 
+@AutoService(ComponentRegistrar::class)
 class TestLoggerComponentRegistrar() : ComponentRegistrar {
 
   override fun registerProjectComponents(
